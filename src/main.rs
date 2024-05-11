6e6d7c651ed58a6871f8b0eb930c514a7ab74d6b
irfanzainudin <m.irfan.zain@gmail.com>
2024-05-11 18:44:00 +1000

    can now read user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::{self, BufRead};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -14,7 +15,8 @@ struct Args {
 }
 
 fn show_game_choices() {
-    let games: Vec<&str> = vec!["Tic Tac Toe", "Chess", "Pacman"];
+    // let games: Vec<&str> = vec!["Tic Tac Toe", "Chess", "Pacman", "Countdown Words"];
+    let games: Vec<&str> = vec!["Countdown Words", "Countdown Numbers"];
     for (i, g) in games.iter().enumerate() {
         println!("{}. {}", i + 1, g);
     }
@@ -27,5 +29,18 @@ fn main() {
         println!("Hello {}!", args.name);
         println!("Please choose what games do you want to play: ");
         show_game_choices();
+        let user_input: Option<Result<String, std::io::Error>> = io::stdin().lock().lines().next();
+        match user_input {
+            None => {}
+            Some(succeed_option) => match succeed_option {
+                Ok(succeed) => {
+                    println!("[SUCCESS] {}", succeed);
+                }
+                Err(failed) => {
+                    println!("[FAILURE] {}", failed);
+                }
+            },
+        }
+        // TODO: need to convert the input to numbers and match that to the game chosen by user
     }
 }
