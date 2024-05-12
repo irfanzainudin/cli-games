a679be1e1bcd0882d6ac994fe07d13d6b37bbb00
irfanzainudin <m.irfan.zain@gmail.com>
2024-05-12 23:32:45 +1000

    match user input to chosen game

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,19 @@ struct Args {
 fn show_game_choices() {
     // let games: Vec<&str> = vec!["Tic Tac Toe", "Chess", "Pacman", "Countdown Words"];
     let games: Vec<&str> = vec!["Countdown Words", "Countdown Numbers"];
+    println!("");
     for (i, g) in games.iter().enumerate() {
         println!("{}. {}", i + 1, g);
     }
+    println!("");
+}
+
+fn countdown_words() {
+    println!("Welcome to Countdown Words!\n");
+}
+
+fn countdown_numbers() {
+    println!("Welcome to Countdown Numbers!\n");
 }
 
 fn main() {
@@ -31,14 +41,36 @@ fn main() {
         show_game_choices();
         let user_input: Option<Result<String, std::io::Error>> = io::stdin().lock().lines().next();
         match user_input {
-            None => {}
+            None => println!("[FAILURE] failed to read user input"),
             Some(succeed_option) => match succeed_option {
                 Ok(succeed) => {
                     // TODO: maybe needs a better implementation
-                    if succeed.chars().all(|nums| nums.is_digit(10)) {
-                        println!("[SUCCESS] {}", succeed);
+                    const RADIX: u32 = 10;
+                    if succeed.chars().all(|nums| nums.is_digit(RADIX)) {
+                        // IMPROVEMENT: have an enum to show which game user chose
+                        println!("[SUCCESS] read {} as chosen game", succeed);
+                        println!("");
+                        // IMPROVEMENT: change next() to an implementation which reads
+                        // ------------ the whole number otherwise the list of games will
+                        // ------------ only accommodate 0-9
+                        let chosen_game = succeed.chars().next().unwrap_or('1');
+                        match chosen_game.to_digit(RADIX) {
+                            None => println!(
+                                "[FAILURE] failed to convert user_input:char to user_input:u32"
+                            ),
+                            Some(chosen_game) => match chosen_game {
+                                1 => countdown_words(),
+                                2 => countdown_numbers(),
+                                otherwise => {
+                                    println!(
+                                        "[FAILURE] no games associated with that number: {}",
+                                        otherwise
+                                    )
+                                }
+                            },
+                        }
                     } else {
-                        println!("[FAILURE] {}", succeed);
+                        println!("[FAILURE] invalid input: {}", succeed);
                     }
                 }
                 Err(failed) => {
