bc5fef20374f501ea9743b3db760e63d982fe0ca
irfanzainudin <m.irfan.zain@gmail.com>
2024-05-16 00:24:02 +1000

    finished a naive version of tictactoe

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ struct Args {
 
 fn show_game_choices() {
     // let games: Vec<&str> = vec!["Tic Tac Toe", "Chess", "Pacman", "Countdown Words"];
-    let games: Vec<&str> = vec!["Countdown Words", "Countdown Numbers"];
+    let games: Vec<&str> = vec!["Tic Tac Toe", "Countdown Words", "Countdown Numbers"];
     println!("");
     for (i, g) in games.iter().enumerate() {
         println!("{}. {}", i + 1, g);
@@ -166,6 +166,189 @@ pub async fn countdown_numbers() {
     println!("What's your answer?");
 }
 
+pub fn print_ttt_board(board: &Vec<Vec<usize>>) {
+    const ROWS: usize = 3;
+    const COLS: usize = 3;
+    const SEPARATOR: usize = 3;
+    for i in 1..=ROWS {
+        for j in 1..=COLS {
+            let vrow_index = i - 1;
+            let vcol_index = j - 1;
+            if board[vrow_index][vcol_index] == 0 {
+                print!("_");
+            } else if board[vrow_index][vcol_index] == 1 {
+                print!("X");
+            } else {
+                print!("O");
+            }
+
+            if j % SEPARATOR == 0 {
+                print!("\n");
+            } else {
+                print!(" | ");
+            }
+        }
+
+        if i != 3 {
+            println!("---------");
+        }
+    }
+}
+
+pub fn ttt_check_winning_conditions(board: &Vec<Vec<usize>>) -> bool {
+    // -- Check for win in rows
+    if board[0][0] != 0 && board[0][0] == board[0][1] && board[0][1] == board[0][2] {
+        return true;
+    } else if board[1][0] != 0 && board[1][0] == board[1][1] && board[1][1] == board[1][2] {
+        return true;
+    } else if board[2][0] != 0 && board[2][0] == board[2][1] && board[2][1] == board[2][2] {
+        return true;
+    }
+
+    // -- Check for win in cols
+    if board[0][0] != 0 && board[0][0] == board[1][0] && board[1][0] == board[2][0] {
+        return true;
+    } else if board[0][1] != 0 && board[0][1] == board[1][1] && board[1][1] == board[2][1] {
+        return true;
+    } else if board[0][2] != 0 && board[0][2] == board[1][2] && board[1][2] == board[2][2] {
+        return true;
+    }
+
+    // -- Check for win in diagonals
+    if board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+        return true;
+    } else if board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+        return true;
+    }
+
+    false
+}
+
+pub fn ttt_computer_move(board: &Vec<Vec<usize>>) -> (usize, usize) {
+    if board[1][1] == 0 {
+        return (1, 1);
+    }
+
+    for (i, r) in board.iter().enumerate() {
+        for (j, c) in r.iter().enumerate() {
+            if *c == 0 {
+                return (i, j);
+            }
+        }
+    }
+
+    return (0, 0);
+}
+
+pub async fn tictactoe() {
+    println!("Welcome to Tic Tac Toe!\n");
+
+    let mut ttt_board: Vec<Vec<usize>> = vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]];
+
+    println!("");
+    print_ttt_board(&ttt_board);
+    println!("");
+    loop {
+        let user_input: Option<Result<String, std::io::Error>> = io::stdin().lock().lines().next();
+        match user_input {
+            None => println!("[FAILURE] failed to read user input"),
+            Some(succeed_option) => match succeed_option {
+                Ok(succeed) => {
+                    let ui = succeed.as_str();
+                    match ui {
+                        "tl" => {
+                            if ttt_board[0][0] == 0 {
+                                ttt_board[0][0] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "tc" => {
+                            if ttt_board[0][1] == 0 {
+                                ttt_board[0][1] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "tr" => {
+                            if ttt_board[0][2] == 0 {
+                                ttt_board[0][2] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "ml" => {
+                            if ttt_board[1][0] == 0 {
+                                ttt_board[1][0] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "mc" => {
+                            if ttt_board[1][1] == 0 {
+                                ttt_board[1][1] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "mr" => {
+                            if ttt_board[1][2] == 0 {
+                                ttt_board[1][2] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "bl" => {
+                            if ttt_board[2][0] == 0 {
+                                ttt_board[2][0] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "bc" => {
+                            if ttt_board[2][1] == 0 {
+                                ttt_board[2][1] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        "br" => {
+                            if ttt_board[2][2] == 0 {
+                                ttt_board[2][2] = 1;
+                            } else {
+                                println!("\n[FAILURE] space already filled\n");
+                            }
+                        }
+                        _ => {
+                            println!("\n[FAILURE] invalid command\n");
+                        }
+                    }
+                }
+                Err(failed) => {
+                    println!("[FAILURE] failed_option: {}", failed);
+                }
+            },
+        }
+
+        // Computer move
+        let c_command = ttt_computer_move(&ttt_board);
+        ttt_board[c_command.0][c_command.1] = 2;
+
+        // Show board
+        println!("");
+        print_ttt_board(&ttt_board);
+        println!("");
+
+        // Check for winning conditions
+        let won = ttt_check_winning_conditions(&ttt_board);
+        if won {
+            break;
+        }
+    }
+
+    println!("Congrats to the winner!");
+}
+
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
@@ -195,8 +378,9 @@ async fn main() {
                                 "[FAILURE] failed to convert user_input:char to user_input:u32"
                             ),
                             Some(chosen_game) => match chosen_game {
-                                1 => countdown_words().await,
-                                2 => countdown_numbers().await,
+                                1 => tictactoe().await,
+                                2 => countdown_words().await,
+                                3 => countdown_numbers().await,
                                 otherwise => {
                                     println!(
                                         "[FAILURE] no games associated with that number: {}",
